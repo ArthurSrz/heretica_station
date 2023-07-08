569a8de79282eea5d0fb21fcc5c03e4ef9598413
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-07-08 12:47:16 +0200

    Update 1_De quoi avez-vous besoin ?.py

--- pages/1_De quoi avez-vous besoin ?.py
+++ pages/1_De quoi avez-vous besoin ?.py
@@ -29,6 +29,21 @@ def main():
     st.title("De quoi avez-vous besoin ?")
     st.subheader('Si vous aviez un deuxième cerveau, sur quelle tâche lui demanderiez-vous de travailler ?')
 
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.button("La collecte de données")
+        st_lottie("https://assets7.lottiefiles.com/packages/lf20_6tQ2In419R.json", width=200)
+    with col2:
+        st.button("L'écriture")
+        st_lottie("https://assets5.lottiefiles.com/packages/lf20_QdH33DmN0r.json", width=200)
+    with col3:
+        st.button("Me sentir mieux")
+        st_lottie("https://assets2.lottiefiles.com/packages/lf20_pz5BGiTCej.json")
+    with col4:
+        st.button("Me superviser")
+        st_lottie("https://assets5.lottiefiles.com/packages/lf20_FAHP63f8vG.json")
+
+
     
     # Example usage
     button_labels = ["La collecte de données", "L'écriture", "Me sentir mieux", "Me superviser"]
