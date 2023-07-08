f5c50aac4a4c6d76cbf692adce7b7d1ea15d973b
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-07-08 15:54:38 +0200

    add external URL

--- pages/1_De quoi avez-vous besoin❓.py
+++ pages/1_De quoi avez-vous besoin❓.py
@@ -7,6 +7,14 @@ from streamlit_extras.switch_page_button import switch_page
 from PIL import Image
 from streamlit_lottie import st_lottie
 import json
+import webbrowser
+
+# Function to open a URL
+def open_url(url):
+    webbrowser.open(url)
+
+
+
 
 # Streamlit app
 def main():
@@ -18,16 +26,24 @@ def main():
 
     col1, col2, col3, col4 = st.columns(4)
     with col1:
-        st.markdown('<p style="text-align: center;"><button>La collecte de données</button></p>', unsafe_allow_html=True)
+        button_clicked = st.button("La collecte de données")
+        if button_clicked:
+            open_url("https://google.com")
         st_lottie("https://assets7.lottiefiles.com/packages/lf20_6tQ2In419R.json", key="col1_lottie", width=200)
     with col2:
-        st.markdown('<p style="text-align: center;"><button>L\'écriture</button></p>', unsafe_allow_html=True)
+        button_clicked = st.button("L'écriture")
+        if button_clicked:
+            open_url("https://example2.com")
         st_lottie("https://assets5.lottiefiles.com/packages/lf20_QdH33DmN0r.json", key="col2_lottie", width=200)
     with col3:
-        st.markdown('<p style="text-align: center;"><button>Me sentir mieux</button></p>', unsafe_allow_html=True)
+        button_clicked = st.button("Me sentir mieux")
+        if button_clicked:
+            open_url("https://example3.com")
         st_lottie("https://assets2.lottiefiles.com/packages/lf20_pz5BGiTCej.json", key="col3_lottie")
     with col4:
-        st.markdown('<p style="text-align: center;"><button>Me superviser</button></p>', unsafe_allow_html=True)
+        button_clicked = st.button("Me superviser")
+        if button_clicked:
+            open_url("https://example4.com")
         st_lottie("https://assets5.lottiefiles.com/packages/lf20_FAHP63f8vG.json", key="col4_lottie")
 
     
