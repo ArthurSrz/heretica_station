ef464eadf12d99258abda047f1c7d345671d8f63
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-07-08 16:44:34 +0200

    Update 1_De quoi avez-vous besoin❓.py

--- pages/1_De quoi avez-vous besoin❓.py
+++ pages/1_De quoi avez-vous besoin❓.py
@@ -22,26 +22,26 @@ def main():
     with col1:
 
         button_md1 = '''
-    <a href="https://www.example.com" class="github-button" aria-label="Open External URL" style="display: inline-block; text-align: center; width: 100%;">La collecte de données</a>
+    <a href="https://app.gather.town/invite?token=jjHpJ9ZER1Gych9WBtT9" class="github-button" aria-label="Open External URL" style="display: inline-block; text-align: center; width: 100%;">La collecte de données</a>
     '''
         st.markdown(button_md1, unsafe_allow_html=True)
         st_lottie("https://assets7.lottiefiles.com/packages/lf20_6tQ2In419R.json", key="col1_lottie", width=200)
 
     with col2:
         button_md2 = '''
-    <a href="https://www.example.com" class="github-button" aria-label="Open External URL" style="display: inline-block; text-align: center; width: 100%;">L'écriture</a>
+    <a href="https://app.gather.town/invite?token=jjHpJ9ZER1Gych9WBtT9" class="github-button" aria-label="Open External URL" style="display: inline-block; text-align: center; width: 100%;">L'écriture</a>
     '''
         st.markdown(button_md2, unsafe_allow_html=True)
         st_lottie("https://assets5.lottiefiles.com/packages/lf20_QdH33DmN0r.json", key="col2_lottie", width=200)
     with col3:
         button_md3 = '''
-    <a href="https://www.example.com" class="github-button" aria-label="Open External URL" style="display: inline-block; text-align: center; width: 100%;">Me sentir mieux</a>
+    <a href="https://app.gather.town/invite?token=jjHpJ9ZER1Gych9WBtT9" class="github-button" aria-label="Open External URL" style="display: inline-block; text-align: center; width: 100%;">Me sentir mieux</a>
     '''
         st.markdown(button_md3, unsafe_allow_html=True)
         st_lottie("https://assets2.lottiefiles.com/packages/lf20_pz5BGiTCej.json", key="col3_lottie")
     with col4:
         button_md4 = '''
-    <a href="https://www.example.com" class="github-button" aria-label="Open External URL" style="display: inline-block; text-align: center; width: 100%;">Me superviser</a>
+    <a href="https://app.gather.town/invite?token=jjHpJ9ZER1Gych9WBtT9" class="github-button" aria-label="Open External URL" style="display: inline-block; text-align: center; width: 100%;">Me superviser</a>
     '''
         st.markdown(button_md4, unsafe_allow_html=True)
         st_lottie("https://assets5.lottiefiles.com/packages/lf20_FAHP63f8vG.json", key="col4_lottie")
