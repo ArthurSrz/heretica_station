a111d985e7dbdb4ed4c211c213193b3055fe128b
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-07-08 12:50:39 +0200

    Update 1_De quoi avez-vous besoin ?.py

--- pages/1_De quoi avez-vous besoin ?.py
+++ pages/1_De quoi avez-vous besoin ?.py
@@ -25,11 +25,15 @@ def main():
         st_lottie("https://assets5.lottiefiles.com/packages/lf20_QdH33DmN0r.json", width=200)
         st.empty()
     with col3:
+        st.empty()
         st.button("Me sentir mieux")
         st_lottie("https://assets2.lottiefiles.com/packages/lf20_pz5BGiTCej.json")
+        st.empty()
     with col4:
+        st.empty()
         st.button("Me superviser")
         st_lottie("https://assets5.lottiefiles.com/packages/lf20_FAHP63f8vG.json")
+        st.empty()
 
     
     next_page = st.button("Suivre Heretica")
