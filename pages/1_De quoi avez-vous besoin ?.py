965cb0e8364e12217cbf949eb12dffbe68ebfe9a
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-07-08 12:49:52 +0200

    Update 1_De quoi avez-vous besoin ?.py

--- pages/1_De quoi avez-vous besoin ?.py
+++ pages/1_De quoi avez-vous besoin ?.py
@@ -15,9 +15,12 @@ def main():
 
     col1, col2, col3, col4 = st.columns(4)
     with col1:
+        st.empty()
         st.button("La collecte de données")
         st_lottie("https://assets7.lottiefiles.com/packages/lf20_6tQ2In419R.json", width=200)
+        st.empty()
     with col2:
+        st.empty()
         st.button("L'écriture")
         st_lottie("https://assets5.lottiefiles.com/packages/lf20_QdH33DmN0r.json", width=200)
     with col3:
