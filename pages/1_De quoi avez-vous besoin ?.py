2b5e4cff1b8770cb2e4f63f99ef322ffb2fcf7a0
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-07-08 11:47:55 +0200

    Update 1_De quoi avez-vous besoin ?.py

--- pages/1_De quoi avez-vous besoin ?.py
+++ pages/1_De quoi avez-vous besoin ?.py
@@ -4,10 +4,10 @@ import pandas as pd
 from io import StringIO
 from streamlit_extras.metric_cards import style_metric_cards
 from streamlit_extras.switch_page_button import switch_page
+from PIL import Image
 
-
-#function to display buttons
-def display_button_gallery(buttons, items_per_row=4):
+#Function to display the gallery
+def display_button_gallery(buttons, images, items_per_row=4):
     num_items = len(buttons)
     num_rows = (num_items - 1) // items_per_row + 1
 
@@ -18,7 +18,9 @@ def display_button_gallery(buttons, items_per_row=4):
 
         for col, index in zip(cols, range(start_index, end_index)):
             button = buttons[index]
+            image = images[index]
             col.button(button)
+            col.image(image, use_column_width=True)
             
 
 # Streamlit app
@@ -27,9 +29,11 @@ def main():
     st.subheader('Si vous aviez un deuxième cerveau, sur quelle tâche lui demanderiez-vous de travailler ?')
 
     
-    # Display
-    button_labels = ['Button 1', 'Button 2', 'Button 3', 'Button 4', 'Button 5', 'Button 6', 'Button 7', 'Button 8']
-    display_button_gallery(button_labels)
+    # Example usage
+    button_labels = ['Button 1', 'Button 2', 'Button 3', 'Button 4']
+    image_paths = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg']
+    images = [Image.open(image_path) for image_path in image_paths]
+    display_button_gallery(button_labels, images)
     
     next_page = st.button("Suivre Heretica")
     if next_page:
